544204b80f5dfb699240d21895c79b4386d7c4a3
tduclos <67517520+tduclos@users.noreply.github.com>
2020-07-06 14:44:59 -0400

    showPage function

--- js/script.js
+++ js/script.js
@@ -5,19 +5,13 @@ FSJS project 2 - List Filter and Pagination
    
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
+   //Global Variables
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+//contains every li in an array
+const listItems = document.querySelector('.student-list').children; 
 
-
+//the max number of elements in a page
+const pageSize = 10;
 
 
 /*** 
@@ -35,7 +29,15 @@ FSJS project 2 - List Filter and Pagination
        "invoke" the function 
 ***/
 
-
+function showPage(list, page){
+        for(let i=0; i<list.length;i++){
+          if((Math.floor(i/pageSize)+1)===page){
+             list[i].style.display = '';
+             }else{
+             list[i].style.display = 'none';    
+             };
+        };
+};
 
 
 /*** 
@@ -43,8 +45,10 @@ FSJS project 2 - List Filter and Pagination
    functionality to the pagination buttons.
 ***/
 
+function appendPageLinks(){
+    
+};
 
-
-
+showPage(listItems, 1);
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
